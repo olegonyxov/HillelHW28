7ef17bee852b8b0b384d87bd9a03298eb8812c7e
olegonyxov <olegonyxov@gmail.com>
2024-04-24 03:01:07 +0300

    3

--- js/app.js
+++ js/app.js
@@ -1,80 +1,71 @@
-const sliderBox = document.getElementById("sliderBox")
-const btnBox = document.createElement('div')
-const prevBtn = document.createElement('button')
-prevBtn.textContent="prev <<"
-const stopBtn = document.createElement('button')
-stopBtn.textContent="stop"
-const nextBtn = document.createElement('button')
-nextBtn.textContent=">> next"
-buttons= [prevBtn,stopBtn,nextBtn]
+const sliderBox = document.getElementById("sliderBox");
+const btnBox = document.createElement('div');
+const prevBtn = document.createElement('button');
+prevBtn.textContent = "prev <<";
+const stopBtn = document.createElement('button');
+stopBtn.textContent = "stop";
+const nextBtn = document.createElement('button');
+nextBtn.textContent = ">> next";
+buttons = [prevBtn, stopBtn, nextBtn];
 buttons.forEach(button => {
-    btnBox.appendChild(button)
+    btnBox.appendChild(button);
 });
 
 
 let imgPath = 1;
 let stopCheck = false;
 
-function checkIndex(){
-    if (imgPath > 5){
-        imgPath = 1
-    } else if (imgPath < 1){
-        imgPath = 5
+function checkIndex() {
+    if (imgPath > 5) {
+        imgPath = 1;
+    } else if (imgPath < 1) {
+        imgPath = 5;
     }
 }
 
-function renderImage(){
-    const myImage = document.createElement("img")
-
+function renderImage() {
+    const myImage = document.createElement("img");
     sliderBox.innerHTML = '';
-    myImage.src=`./img/img${imgPath}.jpg`
-    sliderBox.appendChild(myImage)
-    sliderBox.appendChild(btnBox)
+    myImage.src = `./img/img${imgPath}.jpg`;
+    sliderBox.appendChild(myImage);
+    sliderBox.appendChild(btnBox);
 
 }
 
-function changeSlide(){
-    renderImage()
-    imgPath++
-    checkIndex()
+function changeSlide() {
+    renderImage();
+    imgPath++;
+    checkIndex();
 }
 
-function stopCheckRender(){
+function stopCheckRender() {
     stopCheck = true;
     checkIndex();
     renderImage();
 }
 
-btnBox.addEventListener('click', function(event) {
+btnBox.addEventListener('click', function (event) {
     const target = event.target;
     if (target.tagName === 'BUTTON') {
-        clearInterval(interval3); 
+        clearInterval(interval3);
         if (target === prevBtn) {
             if (stopCheck === false) {
                 imgPath--;
             }
             imgPath--;
-            stopCheckRender()
-            
+            stopCheckRender();
+
         } else if (target === nextBtn) {
             if (stopCheck === true) {
                 imgPath++;
             }
-            stopCheckRender()
+            stopCheckRender();
         }
-        
-        
+
+
     }
 });
 
-changeSlide()
-interval3 = setInterval(changeSlide,3000)
-
-
-
-
-
-
-
-
+changeSlide();
+interval3 = setInterval(changeSlide, 3000);
 
